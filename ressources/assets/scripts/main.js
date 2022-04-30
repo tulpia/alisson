f8b47cb567b7261e646b3c56625d8672925b8294
tulpia <teihotaata.alec@gmail.com>
2022-04-30 13:21:15 +0200

    cablage / inte de la single projet

--- ressources/assets/scripts/main.js
+++ ressources/assets/scripts/main.js
@@ -1,44 +1,45 @@
 // Dependencies
 import Highway from "@dogstudio/highway";
-import LocomotiveScroll from 'locomotive-scroll';
+import LocomotiveScroll from "locomotive-scroll";
 
 // Renderers
 import AccueilRenderer from "./renderers/AccueilRenderer";
 import AboutRenderer from "./renderers/AboutRenderer";
 import ContactRenderer from "./renderers/ContactRenderer";
 import WorksRenderer from "./renderers/WorksRenderer";
+import SingleRenderer from "./renderers/SingleRenderer";
 
 // Transitions
 import DefaultTransition from "./transitions/DefaultTransition";
 
-// eslint-disable-line
-const H = new Highway.Core({
-    renderers: {
-        home: AccueilRenderer,
-        about: AboutRenderer,
-        contact: ContactRenderer,
-        works: WorksRenderer
-
-    },
-    transitions: {
-        default: DefaultTransition
-    }
-});
-
 let scroll;
 
-document.addEventListener("DOMContentLoaded", function() {
-    scroll = new LocomotiveScroll({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        inertia: 0.5
-    });
-});
+document.addEventListener("DOMContentLoaded", function () {
+  // eslint-disable-line
+  const H = new Highway.Core({
+    renderers: {
+      accueil: AccueilRenderer,
+      about: AboutRenderer,
+      contact: ContactRenderer,
+      works: WorksRenderer,
+      single: SingleRenderer,
+    },
+    transitions: {
+      default: DefaultTransition,
+    },
+  });
 
-H.on("NAVIGATE_OUT", () => {
-    scroll.scrollTo(".page");
-});
-  
-H.on("NAVIGATE_END", () => {
+  scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    inertia: 0.5,
+  });
+
+  H.on("NAVIGATE_OUT", () => {
+    scroll.scrollTo("[data-router-view]");
+  });
+
+  H.on("NAVIGATE_END", () => {
     scroll.update();
+  });
 });
--- ressources/assets/scripts/transitions/DefaultTransition.js
+++ ressources/assets/scripts/transitions/DefaultTransition.js
@@ -1,17 +1,55 @@
-import Highway from '@dogstudio/highway';
+import Highway from "@dogstudio/highway/src/highway";
+import { gsap } from "gsap";
+
+const transitionLayer = document.querySelector(".transition-container");
 
 class DefaultTransition extends Highway.Transition {
   // Built-in methods
   in({ from, to, trigger, done }) {
-    // [...]
     from.remove();
-    done();
+
+    const tlIn = gsap.timeline({
+      paused: true,
+    });
+
+    tlIn.fromTo(
+      transitionLayer,
+      {
+        opacity: 1,
+      },
+      {
+        opacity: 0,
+        duration: 0.75,
+        onComplete: () => {
+          done();
+        },
+      }
+    );
+
+    tlIn.play();
   }
 
   out({ from, trigger, done }) {
-    // [...]
-    from.remove();
-    done();
+    const tlOut = gsap.timeline({
+      paused: true,
+      onComplete: () => {
+        // quand le done est appelé, la transition
+        done();
+      },
+    });
+
+    tlOut.fromTo(
+      transitionLayer,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 0.75,
+      }
+    );
+
+    tlOut.play();
   }
 }
 
