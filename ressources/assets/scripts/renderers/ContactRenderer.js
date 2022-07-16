5a6c51d77e7242ccef5cbcda9e2783fc8a4a3296
tulpia <teihotaata.alec@gmail.com>
2022-07-16 13:11:28 +0200

    Anims de la page contact

--- ressources/assets/scripts/renderers/ContactRenderer.js
+++ ressources/assets/scripts/renderers/ContactRenderer.js
@@ -1,12 +1,79 @@
-import Highway from '@dogstudio/highway';
+import Highway from "@dogstudio/highway";
+import Loader from "../classes/Loader";
+import Main from "../main";
+import { gsap } from "gsap";
+import SplitType from "split-type";
 
 class ContactRenderer extends Highway.Renderer {
-    // Hooks/methods
-    onEnter() {}
-    onLeave() {}
-    onEnterCompleted() {}
-    onLeaveCompleted() {}
+  constructor(properties) {
+    super(properties);
+
+    this.tlLanding = gsap.timeline({ paused: true });
+    this.scroll;
   }
-  
-  // Don`t forget to export your renderer
-  export default ContactRenderer;
+
+  onEnter() {
+    this.scroll = Main.initLocomotive();
+
+    //------------------------
+    // ------------ ANIMATIONS
+    //------------------------
+    const title = new SplitType(".page-contact .title", { types: "lines" });
+    const text = document.querySelector(".page-contact__content .introduction");
+    const mail = document.querySelector(".page-contact__content .mail");
+
+    this.tlLanding
+      .fromTo(
+        title.lines,
+        {
+          y: 10,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.5,
+          stagger: 0.085,
+        }
+      )
+      .fromTo(
+        text,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: 0.35,
+        },
+        ">-.35"
+      )
+      .fromTo(
+        mail,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: 0.35,
+        },
+        ">-.15"
+      );
+  }
+
+  onLeave() {
+    this.scroll.destroy();
+  }
+
+  onEnterCompleted() {
+    this.scroll.update();
+
+    Loader.onLoad(() => {
+      this.tlLanding.play();
+    });
+  }
+
+  onLeaveCompleted() {}
+}
+
+// Don`t forget to export your renderer
+export default ContactRenderer;
